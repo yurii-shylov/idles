11e5c8fb1703a84d4cc2abab53f18110c21a6e6a
Yurii Shylov <yurii.shylov@gmail.com>
2016-03-13 00:07:59 +0100

    Prepare deploying to Github Pages

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -18,9 +18,7 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
   // Sets app default base URL
   app.baseUrl = '/';
   if (window.location.port === '') {  // if production
-    // Uncomment app.baseURL below and
-    // set app.baseURL to '/your-pathname/' if running from folder in production
-    // app.baseUrl = '/polymer-starter-kit/';
+    app.baseUrl = '/idles/';
   }
 
   app.displayInstalledToast = function() {
