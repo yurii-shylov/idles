fdc17d81ec11bf2fef28f8218e1436323730b996
Yurii Shylov <yurii.shylov@gmail.com>
2016-03-15 23:55:25 +0100

    Reenable https and use it for local server also

--- gulpfile.js
+++ gulpfile.js
@@ -228,10 +228,7 @@ gulp.task('serve', ['styles', 'elements'], function() {
         }
       }
     },
-    // Run as an https by uncommenting 'https: true'
-    // Note: this uses an unsigned certificate which on first access
-    //       will present a certificate warning in the browser.
-    // https: true,
+    https: true,
     server: {
       baseDir: ['.tmp', 'app'],
       middleware: [historyApiFallback()]
